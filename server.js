d860ea22a031cd21aedf3167a6dd57cc1445f0de
domu.harahap.dynatrace@gmail.com <domu.harahap.dynatrace@gmail.com>
2026-09-21 14:57:22 +0800

    update

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'));
